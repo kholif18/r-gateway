61faad6680516f1a6304096f22d27c16d5fdc652
kholif18 <rahmadnur.kholifatur@gmail.com>
2025-07-12 21:57:14 +0700

    http/https

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+
+class TrustProxies extends Middleware
+{
+    protected $proxies = '*';
+    protected $headers = 31;
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use App\Models\Setting;
 use Illuminate\Routing\Router;
 use App\Services\WhatsAppService;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
@@ -40,6 +42,17 @@ class AppServiceProvider extends ServiceProvider
             view()->share('app_version', Setting::getGlobal('app_version', config('app.version')));
         } else {
             view()->share('app_version', config('app.version'));
+        }
+
+        $appUrl = config('app.url');
+
+        Log::info('APP URL from config:', ['url' => $appUrl]);
+
+        if (str_starts_with($appUrl, 'https://')) {
+            URL::forceScheme('https');
+            Log::info('✅ Force HTTPS activated via config(app.url)');
+        } else {
+            Log::warning('⚠️ HTTPS NOT activated, config(app.url): ' . $appUrl);
         }
     }
 }
